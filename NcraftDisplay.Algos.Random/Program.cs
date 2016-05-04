af529def9b0c03829752338de23b2aaaf15b8160
Rémi Lesieur <sensei.seta@gmail.com>
2016-05-04 17:34:07 +0200

    Rajout d'un algo random (pour la rigoulade).

---
+++ NcraftDisplay.Algos.Random/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace NcraftDisplay.Algos.Random
+{
+    class Program
+    {
+        private static string[] alpha = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"};
+
+        static void Main(string[] args)
+        {
+            var l = string.Empty;
+
+            while (l != "EMPTY")
+            {
+                var randomizer = GetRandom();
+                Console.WriteLine(randomizer);
+
+                l = Console.ReadLine();
+            }
+        }
+
+        private static string GetRandom()
+        {
+            var random = new System.Random((int) DateTime.Now.Ticks);
+
+            var x = random.Next(0, 9);
+            var y = random.Next(0, 9);
+
+            return string.Format("{0}{1}", alpha[x], y);
+        }
+    }
+}
